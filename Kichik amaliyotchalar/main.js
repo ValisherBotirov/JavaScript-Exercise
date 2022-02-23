477cbc0df6cd6adb7bae3c37bd0d2079c4006218
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-02-23 16:19:54 +0500

    Strict mode in using

---
+++ Kichik amaliyotchalar/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let age = 20;
+age < 22 ? console.log("20 dan kichik") : console.log("20 dan katta");
+
+let foo = 20;
+let too = true;
+
+foo < 30 && foo > 18
+  ? console.log("Qabul qilindingiz")
+  : foo <= 17
+  ? console.log("Yoshiz jud kichik ekan")
+  : console.log("Yoshingiz juda katta ekan");
