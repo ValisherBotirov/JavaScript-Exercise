007232b272e845f8f6f4b0dfcda34129fb572b7e
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-02-25 14:11:43 +0500

    Tahrirladim

--- Kichik amaliyotchalar/script.js
+++ Kichik amaliyotchalar/script.js
@@ -167,33 +167,3 @@
 // };
 
 // calcAge();
-// <!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="UTF-8" />
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//     <title>Document</title>
-//     <script src="/script.js"></script>
-//     <script src="/main.js"></script>
-//     <script src="./uyda.js"></script>
-//   </head>
-//   <body>
-//     <h1>JavaScript exirsece</h1>
-//   </body>
-// </html>
-// <!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="UTF-8" />
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//     <title>Document</title>
-//     <script src="/script.js"></script>
-//     <script src="/main.js"></script>
-//     <script src="./uyda.js"></script>
-//   </head>
-//   <body>
-//     <h1>JavaScript exirsece</h1>
-//   </body>
-// </html>
