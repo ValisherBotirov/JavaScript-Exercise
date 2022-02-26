157477facee94570002409c355f8cabe27c2351c
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-02-26 17:30:48 +0500

    Kiritilgan sonlarni necha xonaligini va raqamlarini yig'indisini topuvchi masalani yechdim

--- QUDRAT ABDURAHIMOV (Masalalar to'plami/3.Mantiqiy amallarga oid masalalar/1-masala[]/script.js
+++ QUDRAT ABDURAHIMOV (Masalalar to'plami/3.Mantiqiy amallarga oid masalalar/1-masala[]/script.js
@@ -16,17 +16,17 @@ console.log(Boolean(a % 2 == 0 || b % 2 == 0));
 //  Objectlarni birlashtirish
 // ========================================
 
-let user = {
-  name: "Valisher",
-};
-let userAge = {
-  age: 20,
-};
-let userJob = {
-  job: "Fullstact Developer",
-};
+// let user = {
+//   name: "Valisher",
+// };
+// let userAge = {
+//   age: 20,
+// };
+// let userJob = {
+//   job: "Fullstact Developer",
+// };
 
-console.log(Object.assign(user, userAge, userJob));
+// console.log(Object.assign(user, userAge, userJob));
 
 // let arr = [15, -5, 2, -9, -8, 65, -5, -7, 8, -10];
 // let count = 0;
@@ -36,3 +36,13 @@ console.log(Object.assign(user, userAge, userJob));
 //   }
 // }
 // console.log(count);
+// ===================================================
+// Kiritilgan sonning raqamlar yig'indisini topadigan son
+let number = prompt((arr = []));
+console.log(`Kiritilgan sonning raqamlar soni:  ${number.length}`);
+let sum = 0;
+for (let i = 0; i < number.length; i++) {
+  sum = sum + Number(number[i]);
+}
+console.log(`Kiritilgan sonning raqamlar yig'indisi:  ${sum}`);
+// ==========================================
