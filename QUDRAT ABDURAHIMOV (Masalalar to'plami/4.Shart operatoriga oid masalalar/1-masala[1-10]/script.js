cde3f0e1db9d845ec6b4341b8a52bb8ef25dc9ad
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-02-27 14:37:07 +0500

    Masala

---
+++ QUDRAT ABDURAHIMOV (Masalalar to'plami/4.Shart operatoriga oid masalalar/1-masala[1-10]/script.js
@@ -0,0 +1,44 @@
+// 1-masala
+// let son = -5;
+// if (son > 0) {
+//   son++;
+//   console.log(son);
+// } else console.log(son);
+
+// 2-masala
+// let son = 5;
+// if (son > 0) {
+//   son++;
+//   console.log(son);
+// } else {
+//   son -= 2;
+//   console.log(son);
+// }
+
+// 3-masala
+// let son = 0;
+// if (son > 0) {
+//   son++;
+// } else if (son < 0) {
+//   son -= 2;
+// } else son = 10;
+// console.log(son);
+
+// 4-masala
+// let arr = [10, -5, 25, -5, -4];
+// let count = 0;
+// for (i = 0; i < arr.length; i++) {
+//   if (arr[i] < 0) {
+//     count++;
+//   }
+// }
+// console.log(count);
+
+// =======Dars davomidagi amaliyotchalar=============
+let string = "Valisher";
+console.log(string.indexOf("a"));
+console.log(string.lastIndexOf("a"));
+console.log(string.startsWith("V"));
+console.log(string.endsWith("r"));
+console.log(string.slice(-4, 9));
+// =========================================
