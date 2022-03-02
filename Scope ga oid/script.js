5dc2ccc792274e5407dff13631c52106b3d6ad79
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-03-02 16:20:48 +0500

    Scope turlariga oid mashg'ulot

---
+++ Scope ga oid/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const myName = "Valisher";
+
+function age() {
+  const birtYear = 2002;
+
+  function calcAge() {
+    let job = "Developer";
+    console.log(`My name is ${myName} and  my age ${2022 - birtYear}`);
+  }
+  calcAge();
+  age();
+}
+// age();
