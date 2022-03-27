8cbb13733aff41e05573e5d391a700628898d92a
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-03-27 14:22:38 +0500

    RoboContest masalalari

--- RoboContest masalalar/style.js
+++ RoboContest masalalar/style.js
@@ -10,10 +10,24 @@
 // console.log(sum);
 
 // 462-masala
-let str = "sherzod";
-for (let i = 0; i < str.length; i++) {
-  if (str[i] == "s" && str[i + 1] == "h") {
-    console.log(str[i] + str[i + 1]);
-    0;
-  } else console.log(str[i]);
+// let str = "sherzod";
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i] == "s" && str[i + 1] == "h") {
+//     console.log(str[i] + str[i + 1]);
+//     0;
+//   } else console.log(str[i]);
+// }
+
+// 460-masala
+let n = 1;
+let sum = 0;
+for (let i = 0; i <= n; i++) {
+  for (let j = 0; j <= n; j++) {
+    for (let k = 0; k <= n; k++) {
+      if (i + j + k == n) {
+        sum++;
+      }
+    }
+  }
 }
+console.log(sum);
