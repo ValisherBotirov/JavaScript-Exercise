a8f4afd37397d9b17c6e658d2900434139eb0892
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-04-09 18:46:11 +0500

    Regeneretor-runtime fungsiyasini yuklab oldim

--- Modern JavaScript(practika)/script.js
+++ Modern JavaScript(practika)/script.js
@@ -73,14 +73,5 @@ const by = require('./by.js');
 console.log(by.a);
 by.kir('Valisher', 1111);
 
-// var map = L.map('map').setView([51.505, -0.09], 13);
-
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   attribution:
-//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-// }).addTo(map);
-
-// L.marker([51.5, -0.09])
-//   .addTo(map)
-//   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//   .openPopup();
+import 'core-js/stable';
+import 'regenerator-runtime';
--- QUDRAT ABDURAHIMOV (Masalalar to'plami/7.While sikl operatoriga oid masalalar/script.js
+++ QUDRAT ABDURAHIMOV (Masalalar to'plami/7.While sikl operatoriga oid masalalar/script.js
@@ -20,3 +20,23 @@
 //   result++;
 // }
 // console.log(result);
+
+// Eng ko'p elemt qatnashgan array
+
+let arr = [
+  1, 2, 3, 4, 5, 5, 6, 78, 3, 2, 1, 5, 2, 3, 4, 5, 6, 5, 7, 1, 5, 3, 5,
+];
+
+let max = 0;
+for (let i = 0; i < arr.length; i++) {
+  let count = 0;
+  for (let j = i; j < arr.length; j++) {
+    if (arr[i] === arr[j]) {
+      count++;
+    }
+  }
+  if (max < count) {
+    max = count;
+    console.log(max, arr[i]);
+  }
+}
