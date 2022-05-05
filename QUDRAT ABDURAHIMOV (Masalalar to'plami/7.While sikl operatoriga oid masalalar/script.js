d779f1796142ced7254a90f09a9bbed254e615ca
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-05-05 08:57:46 +0500

    Qudrat Abduhakimov masalalar to'lami While sikliga oid 24-masala

--- QUDRAT ABDURAHIMOV (Masalalar to'plami/7.While sikl operatoriga oid masalalar/script.js
+++ QUDRAT ABDURAHIMOV (Masalalar to'plami/7.While sikl operatoriga oid masalalar/script.js
@@ -267,19 +267,19 @@
 //   }
 // }
 
-// Fibonachi sonlar
-// let n = 20;
-// let k1 = 1,
-//   k2 = 1;
-// let i = 3;
-// let kn = k2 + k1;
-// while (n > kn) {
-//   kn = k1 + k2;
-//   console.log(kn);
-//   k1 = i - 1;
-//   k2 = i - 2;
-//   i++;
-// }
+Fibonachi sonlar
+let n = 20;
+let k1 = 1,
+  k2 = 1;
+let i = 3;
+let kn = k2 + k1;
+while (n > kn) {
+  kn = k1 + k2;
+  console.log(kn);
+  k1 = i - 1;
+  k2 = i - 2;
+  i++;
+}
 
 // 24-masala
 
