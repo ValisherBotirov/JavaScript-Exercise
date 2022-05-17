38ac4abc339b2ded2ecad9a3f46a7e9ccdb74693
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-05-17 17:21:52 +0500

    Qudrat Abduhakimov masalalar to'lami Min va Max sonlarga oid 14-masala

--- QUDRAT ABDURAHIMOV (Masalalar to'plami/8.Minumum va maxsimum sonlarga oid masalalar/script.js
+++ QUDRAT ABDURAHIMOV (Masalalar to'plami/8.Minumum va maxsimum sonlarga oid masalalar/script.js
@@ -1,11 +1,26 @@
 // 1-masala
-let m = 6;
-const arrA = [1, 2, 3, 4, 5, 6];
-const arrB = [1, 2, 3, 4, 5, 6];
-let max = arrA[0] * arrB[0];
-for (let i = 1; i <= m; i++) {
-  if (arrA[i] * arrB[i] > max) {
-    max = arrA[i] * arrB[i];
+// let m = 6;
+// const arrA = [1, 2, 3, 4, 5, 6];
+// const arrB = [1, 2, 3, 4, 5, 6];
+// let max = arrA[0] * arrB[0];
+// for (let i = 1; i <= m; i++) {
+//   if (arrA[i] * arrB[i] > max) {
+//     max = arrA[i] * arrB[i];
+//   }
+// }
+// console.log(max);
+
+// 14-masala
+let b = 60;
+let index = "";
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > b) {
+    index = arr[i];
+    break;
   }
 }
-console.log(max);
+
+if (index) {
+  console.log(index);
+} else console.log("00");
