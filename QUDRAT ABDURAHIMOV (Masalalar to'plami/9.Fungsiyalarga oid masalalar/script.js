79455b24ddc563df78c32769bb1b9092ab1972c1
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-05-23 01:13:58 +0500

    Qudrat Abduhakimov masalalar to'plami Fungsiyalarga oid bobdan 6-masala arrow function usulida

--- QUDRAT ABDURAHIMOV (Masalalar to'plami/9.Fungsiyalarga oid masalalar/script.js
+++ QUDRAT ABDURAHIMOV (Masalalar to'plami/9.Fungsiyalarga oid masalalar/script.js
@@ -1,1 +1,15 @@
-console.log("Hello Guys");
+// 6-masala
+let x = 123456;
+x = x.toString();
+let count = 0;
+let sum = 0;
+
+digitCountFunc = (number) => {
+  number = number.toString();
+  for (let i = 0; i < number.length; i++) {
+    sum += +number[i];
+    count++;
+  }
+  return sum;
+};
+console.log(digitCountFunc(123456));
